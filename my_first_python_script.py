ef9acdc73c660c6fd6141b70959f0d4333ce016b
dohardthings <ithielcurry.nlis@gmail.com>
2023-09-03 21:36:26 +0100

    project work 2

---
+++ my_first_python_script.py
@@ -0,0 +1,14 @@
+# creates a string data type variable to store the user's name
+name = "Ithiel"
+
+# creates a integer data type to store user's age
+age = 14
+
+# create another string data type variable to store user's hobby
+hobby = "basketball"
+
+print("My name is",name)
+print("I am",age,"years old")
+print("i Love",hobby)
+
+
