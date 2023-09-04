5a9128cb41202461e14d070c43bdc2c03a2b885b
dohardthings <ithielcurry.nlis@gmail.com>
2023-09-04 10:03:03 +0100

    added project work 3

---
+++ python_intro.py
@@ -0,0 +1,9 @@
+Name = str(input("Enter your name: "))
+print("Hello",Name,"Welcome to the world of python programming")
+
+Age = int(input("How old are you: "))
+Year = int(2023)
+
+print("Judging from your age you were born in the year: ",Year - Age)
+
+print("Thank you for participating")
